70fc63bc62a967e80184a933d3c36ce18eacb2f3
Tomeu Ramis <tomeuramis99@yahoo.es>
2020-05-15 20:05:27 +0200

    hola

--- PracticasIG/Etapa 3/escena_3d.cpp
+++ PracticasIG/Etapa 3/escena_3d.cpp
@@ -8,8 +8,8 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <iostream>
-const int W_WIDTH = 500; // Tama�o incial de la ventana
-const int W_HEIGHT = 500;
+const int W_WIDTH = 1700; // Tama�o incial de la ventana
+const int W_HEIGHT = 800;
 GLfloat fAngulo; // Variable que indica el �ngulo de rotaci�n de los ejes. 
 float p_width = 500, p_height = 500;
 
@@ -20,7 +20,7 @@ void myResize(int width, int height) {
 	float nr_w = width / p_width;
 	float nr_h = height / p_height;
 
-	glOrtho(-nr_w, nr_w, -nr_h, nr_h, -1, 1);
+	glOrtho(-nr_w, nr_w, -nr_h, nr_h, 1, -1);
 
 	p_width = width;
 	p_height = height;
@@ -34,36 +34,127 @@ void Display(void)
 
 	glPushMatrix();
 
+	//Activamos buffer de PROFUNDIAD (modo sexo: on)
+	glEnable(GL_DEPTH_TEST);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+
+	//Colores sobras y cosas divertidas
+	/*glEnable(GL_LIGHTING);
+	glEnable(GL_COLOR_MATERIAL);
+	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+
+	glEnable(GL_LIGHT0);
+
+	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 1);*/
 
 	// Rotamos las proximas primitivas
-	glRotatef(fAngulo, 0.0f, 0.0f, 1.0f);
-	// Creamos a continuaci�n dibujamos los tres poligonos
-	glBegin(GL_POLYGON);
-	glColor3f(1.0f, 1.0f, 1.0f);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(1.0f, 0.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(-0.5f, 0.866f, 0.0f);
+	glRotatef(45, 1.0f, 0.0f, 0.0f);
+	glRotatef(fAngulo, 0.0f, 1.0f, 0.0f);
+	//glRotatef(45, 0.0f, 1.0f, 0.0f);
+
+	//Ejes de cordenadas
+	glLineWidth(3);
+
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0, 0);
+	glVertex3f(-20, 0, 0);
+	glVertex3f(20, 0, 0);
 	glEnd();
 
-	glBegin(GL_POLYGON);
-	glColor3f(1.0f, 1.0f, 1.0f);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(1.0f, 0.0f, 0.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(-0.5f, -0.866f, 0.0f);
+	glBegin(GL_LINES);
+	glColor3f(0, 1.0f, 0);
+	glVertex3f(0, -20, 0);
+	glVertex3f(0, 20, 0);
 	glEnd();
 
-	glBegin(GL_POLYGON);
-	glColor3f(1.0f, 1.0f, 1.0f);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 1.0f);
-	glVertex3f(-0.5f, 0.866f, 0.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(-0.5f, -0.866f, 0.0f);
+	glBegin(GL_LINES);
+	glColor3f(0, 0, 1.0f);
+	glVertex3f(0, 0, -20);
+	glVertex3f(0, 0, 20);
 	glEnd();
+
+	glLineWidth(1);
+
+	for (float i = -20; i <= 20; i += 0.25) {
+		glBegin(GL_LINES);
+		glColor3f(0, 0, 0);
+		glVertex3f(i, 0, -20);
+		glVertex3f(i, 0, 20);
+		glEnd();
+		glBegin(GL_LINES);
+		glColor3f(0, 0, 0);
+		glVertex3f(-20, 0, i);
+		glVertex3f(20, 0, i);
+		glEnd();
+	}
+
+	//Objetos
+
+	//Teapod
+	glPushMatrix();
+	glTranslatef(-0.7, 0.25, -0.7);
+
+	glColor3f(0.5, 0.5, 0.5);
+	glutSolidTeapot(0.4);
+	glColor3f(0, 0, 0);
+	glutWireTeapot(0.4);
+
+	glPopMatrix();
+
+	//theCube
+	glPushMatrix();
+	glTranslatef(-0.5, 0.25, 0.5);
+
+	glColor3f(0.5, 0.5, 0.5);
+	glutSolidCube(0.5);
+
+	glColor3f(0, 0, 0);
+	glutWireCube(0.5);
+
+	glPopMatrix();
+
+	//Torus
+	glPushMatrix();
+
+	glTranslatef(0.7, 0.15, -0.7);
+	glRotatef(90, 1.0f, 0, 0);
+
+	glColor3f(0.5, 0.5, 0.5);
+	glutSolidTorus(0.15, 0.3, 50, 50);
+
+	glColor3f(0, 0, 0);
+	glutWireTorus(0.15, 0.3, 50, 50);
+
+	glPopMatrix();
+
+	//Cone
+	glPushMatrix();
+	glTranslatef(0.5, 0, 0.5);
+	glRotatef(-90, 1, 0, 0);
+
+	glColor3f(0.5, 0.5, 0.5);
+	glutSolidCone(0.2, 1, 50, 50);
+
+	glColor3f(0, 0, 0);
+	glutWireCone(0.2, 1, 50, 50);
+
+	glPopMatrix();
+
+	
+
+
+	//Dibujamos el "suelo" que es trans
+	glBegin(GL_POLYGON);
+	glColor4f(0.5f, 0.5f, 1.0f, 0.5f);
+	glVertex3f(-20, 0, -20);
+	glVertex3f(20, 0, -20);
+	glVertex3f(20, 0, 20);
+	glVertex3f(-20, 0, 20);
+	glEnd();
+
 	glPopMatrix();
 
 	glutSwapBuffers();
@@ -86,7 +177,7 @@ int main(int argc, char** argv)
 {
 	// Inicializamos la librer�a GLUT
 	glutInit(&argc, argv);
-
+	
 	// Indicamos como ha de ser la nueva ventana
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(W_WIDTH, W_HEIGHT);
@@ -102,7 +193,8 @@ int main(int argc, char** argv)
 
 	// El color de fondo ser� el negro (RGBA, RGB + Alpha channel)
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glOrtho(-1.0, 1.0f, -1.0, 1.0f, -1.0, 1.0f);
+	glOrtho(-1.0, 1.0f, -1.0, 1.0f, -10.0, 10.0f);
+	
 
 	// Comienza la ejecuci�n del core de GLUT
 	glutMainLoop();
