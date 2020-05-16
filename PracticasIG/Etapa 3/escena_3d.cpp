b5b7946ac760ac32755b051a84f08623a688e301
Tomeu Ramis <tomeuramis99@yahoo.es>
2020-05-16 14:31:22 +0200

    transformaciones y luces

--- PracticasIG/Etapa 3/escena_3d.cpp
+++ PracticasIG/Etapa 3/escena_3d.cpp
@@ -13,6 +13,74 @@ const int W_HEIGHT = 700;
 GLfloat fAngulo; // Variable que indica el �ngulo de rotaci�n de los ejes. 
 float p_width = W_WIDTH, p_height = W_HEIGHT;
 
+//Movimiento objetos
+float cubo = 1;
+bool incremento = false;
+
+float cone = 1;
+bool loong = true;
+
+float torus = 0;
+bool trans = true;
+
+//Transformaciones de los objetos
+void rotacionCubo() {
+	if (incremento) {
+		cubo += 0.02;
+		if (cubo >= 0.2) {
+			incremento = false;
+		}
+	}
+	else {
+		cubo += -0.02;
+		if (cubo <= -1.9) {
+			incremento = true;
+		}
+	}
+	glTranslatef(0.5 * cubo, 0, 0);
+	glRotatef(90, 0, 1, 0);
+	glRotatef(100 * cubo, 1, 0, 0);
+}
+
+void looongCone() {
+	if (loong) {
+		cone += 0.01;
+		if (cone >= 3) {
+			loong = false;
+		}
+	}
+	else {
+		cone += -0.01;
+		if (cone <= 0.5) {
+			loong = true;
+		}
+	}
+	glScalef(1, 1, cone);
+}
+
+void destruirDonut() {
+	if (trans) {
+		torus += 0.008;
+		if (torus >= 0.75) {
+			trans = false;
+		}
+	}
+	else {
+		torus -= 0.008;
+		if (torus <= -0.5) {
+			trans = true;
+		}
+	}
+	float shear[] = {
+   1, torus, 0, 0,
+   torus, 1, 0, 0,
+	0, 0, 1, 0,
+	0, 0, 0, 1 };
+	glMultMatrixf(shear);
+}
+
+
+
 void myResize(int width, int height) {
 
 	glViewport(0, 0, width, height);
@@ -45,14 +113,36 @@ void Display(void)
 	glEnable(GL_BLEND);
 
 	//Colores sobras y cosas divertidas
-	/*glEnable(GL_LIGHTING);
-	glEnable(GL_COLOR_MATERIAL);
+	/*He intentado hacer cosas con las luces*/
+	glEnable(GL_LIGHTING);
+	
 	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+	glEnable(GL_COLOR_MATERIAL);
+
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat mat_shininess[] = { 5.0 };
+	GLfloat light_position[] = { 2.0, 0.0, 0.0, 1.0 };
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
+	glShadeModel(GL_SMOOTH);
+
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
+	
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	
+	GLfloat ambient[] = { 0.5, 0.5, 0.5, 1.00};
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+	//GLfloat direction[] = { 0, 0, 1 };
+	//glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, direction);
+	GLfloat light_specular[] = { 10, 10, 10, 1.0 };
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	GLfloat light_diffuse[] = { 1, 1, 1, 1.0 };
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+
 
 	glEnable(GL_LIGHT0);
-
-	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 1);*/
-
+	
 	// Rotamos las proximas primitivas
 	//glRotatef(-45, 1.0f, 0.0f, 0.0f);
 	//glRotatef(fAngulo, 0.0f, 1.0f, 0.0f);
@@ -95,35 +185,36 @@ void Display(void)
 	}
 
 	//Objetos
-
-	
 	
 	//Teapod
 	glPushMatrix();
 
-	glTranslatef(1, 0.7, 1);
-
+	glTranslatef(-0.5, 0.0, -0.8);
 
 	glRotatef(fAngulo * 5, 0.0f, 1.0f, 0.0f);
 	glTranslatef(-0.40, 0.7, 0);
 	glRotatef(30, 0.0f, 0.0f, 1.0f);
+	//glTranslatef(1, 0, 1);
 
 	glColor3f(0.5, 0.5, 0.5);
 	glutSolidTeapot(0.4);
-	glColor3f(0, 0, 0);
-	glutWireTeapot(0.4);
+	//glColor3f(0, 0, 0);
+	//glutWireTeapot(0.4);
 
+	
 	glPopMatrix();
 
 	//theCube
 	glPushMatrix();
 	glTranslatef(-0.5, 0.25, 0.5);
 
+	rotacionCubo();
+
 	glColor3f(0.5, 0.5, 0.5);
 	glutSolidCube(0.5);
 
-	glColor3f(0, 0, 0);
-	glutWireCube(0.5);
+	//glColor3f(0, 0, 0);
+	//glutWireCube(0.5);
 
 	//glMatrixMode(GL_MODELVIEW);
 	glPopMatrix();
@@ -134,11 +225,13 @@ void Display(void)
 	glTranslatef(0.7, 0.15, -0.7);
 	glRotatef(90, 1.0f, 0, 0);
 
+	destruirDonut();
+
 	glColor3f(0.5, 0.5, 0.5);
 	glutSolidTorus(0.15, 0.3, 50, 50);
 
-	glColor3f(0, 0, 0);
-	glutWireTorus(0.15, 0.3, 50, 50);
+	//glColor3f(0, 0, 0);
+	//glutWireTorus(0.15, 0.3, 50, 50);
 
 	glPopMatrix();
 
@@ -147,20 +240,21 @@ void Display(void)
 	glTranslatef(0.5, 0, 0.5);
 	glRotatef(-90, 1, 0, 0);
 
+	looongCone();
+
 	glColor3f(0.5, 0.5, 0.5);
 	glutSolidCone(0.2, 1, 50, 50);
 
-	glColor3f(0, 0, 0);
-	glutWireCone(0.2, 1, 50, 50);
+	//glColor3f(0, 0, 0);
+	//glutWireCone(0.2, 1, 50, 50);
 
 	glPopMatrix();
-
-	
 
 
 	//Dibujamos el "suelo" que es trans
 	glBegin(GL_POLYGON);
-	glColor4f(0.5f, 0.5f, 1.0f, 0.5f);
+	//glColor4f(0.5f, 0.5f, 1.0f, 0.5f);
+	glColor3f(0.5f, 0.5f, 1.0f);
 	glVertex3f(-20, 0, -20);
 	glVertex3f(20, 0, -20);
 	glVertex3f(20, 0, 20);
@@ -191,13 +285,28 @@ int main(int argc, char** argv)
 {
 	// Inicializamos la librer�a GLUT
 	glutInit(&argc, argv);
+
+	
 	
 	// Indicamos como ha de ser la nueva ventana
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(W_WIDTH, W_HEIGHT);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
 
-	
+	//Codigo copiado para las luces
+	/*GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat mat_shininess[] = { 50.0 };
+	GLfloat light_position[] = { 100.0, 100.0, 1.0, 0.0 };
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_SMOOTH);
+
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);*/
 
 	// Creamos la nueva ventana
 	glutCreateWindow("Mis cosas 3D");
@@ -223,3 +332,4 @@ int main(int argc, char** argv)
 	glutMainLoop();
 	return 0;
 }
+
